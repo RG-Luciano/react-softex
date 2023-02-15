c7335639735ff440ed6a7e94a3f9cc2ffa32ace9
Luciano Ramos Garcia <83316284+RG-Luciano@users.noreply.github.com>
2023-02-15 12:17:49 -0300

    Add files via upload

---
+++ atividade4/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Frase from './components/Frase';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName';
+function App() {
+  return (
+    <div className="App">
+     <SayMyName nome="Luciano"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ atividade4/src/components/Frase.js
@@ -0,0 +1,8 @@
+function Frase(){
+    return(
+        <div>
+            <p> Este é um componente de frase</p>
+        </div>
+    )
+}
+export default Frase
